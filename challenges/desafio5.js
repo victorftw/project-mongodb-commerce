53490145b54a2855c5cb16f152f1f9b665670dcd
Victor Hugo <victor.hiraiftw@outlook.com>
2023-03-15 16:11:49 -0300

    feat: query

---
+++ challenges/desafio5.js
@@ -0,0 +1,11 @@
+db.produtos.find(
+  {
+    $or: [{ curtidas: { $eq: 36 } }, { vendidos: { $eq: 85 } }],
+  },
+  {
+    nome: true,
+    vendidos: true,
+    curtidas: true,
+    _id: false,
+  },
+);
