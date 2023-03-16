25f3361182559821799f38d394520e048f12a6e8
Victor Hugo <victor.hiraiftw@outlook.com>
2023-03-16 16:32:03 -0300

    feat: outra query para adicionar uma nova tag no produto de acordo com o percentual de sodio

---
+++ challenges/desafio26.js
@@ -0,0 +1,11 @@
+db.produtos.updateMany({
+  "valoresNutricionais.2.percentual": { $gt: 20, $lt: 40 },
+}, {
+  $push: { tags: { $each: ["contém sódio"] } },
+});
+
+db.produtos.find({}, {
+  nome: true,
+  tags: true,
+  _id: false,
+});
